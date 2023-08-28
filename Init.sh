d10841869629c39eb0bd7e7f5bdc4f2d15de9154
Luis <43306068+Luisda2705@users.noreply.github.com>
2023-08-28 14:59:41 -0600

    Create Init.sh

---
+++ Init.sh
@@ -0,0 +1,10 @@
+cd $HOME
+git clone https://github.com/Luisda2705/bashrc.git
+git clone https://github.com/Luisda2705/wordlists.git
+
+cd bashrc
+mv bashrc $HOME/.bashrc
+
+cd $HOME
+cd wordlists
+./wordlists.sh
