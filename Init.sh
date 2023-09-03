d00a0318017d736d9cfb8a3cb2013fdd3f3e7c19
Luis <43306068+Luisda2705@users.noreply.github.com>
2023-09-02 19:33:07 -0600

    Update Init.sh

--- Init.sh
+++ Init.sh
@@ -1,13 +1,13 @@
-$HOME
+cd $HOME
 git clone https://github.com/Luisda2705/bashrc.git
 git clone https://github.com/Luisda2705/wordlists.git
 
 cd bashrc
 mv bashrc $HOME/.bashrc
 
-$HOME
+cd $HOME
 cd wordlists
 mv wordlists.sh $HOME
 
-$HOME
+cd $HOME
 /bin/bash wordlists.sh
