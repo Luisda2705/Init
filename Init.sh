92449a0938e08877ea3a7954de0fe871cf00e25b
root <root@Debian>
2023-09-27 03:49:19 +0000

    update

--- Init.sh
+++ Init.sh
@@ -3,19 +3,22 @@ git clone https://github.com/Luisda2705/bashrc.git
 git clone https://github.com/Luisda2705/wordlist.git
 git clone https://github.com/Luisda2705/Tools.git
 
+
 cd bashrc
-mv bashrc $HOME/.bashrc
+cp bashrc $HOME/.bashrc
+
 
 cd $HOME
 cd wordlist
 bash Wordlists.sh
 
+
 cd $HOME
 rm bashrc/ && wordlist/ && Init/ && Tools/
 
-useradd -m lnx
-passwd lnx
-usermod -aG sudo lnx
+useradd -m ln
+passwd ln
+usermod -aG sudo ln
 
 cp $HOME/.bashrc  /home/linux/.bashrc
 
