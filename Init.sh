946d14af1f9615338bbddbacba66b2f59b707013
root <root@a78acc1e59fb>
2024-01-27 20:19:24 +0000

    14:19:24 - 27/01/24

--- Init.sh
+++ Init.sh
@@ -6,19 +6,20 @@ git clone https://github.com/Luisda2705/Tools.git
 cd bashrc
 cp bashrc $HOME/.bashrc
 
-cd $HOME
-cd Wordlist
-/bin/bash Wordlists.sh
-
 cp $HOME/.bashrc  /home/lnx/.bashrc
 
 cd $HOME
 cd Tools/
 /bin/bash Tools.sh
 
-cd $HOME
-rm bashrc/ && Wordlist/ && Init/ && Tools/
 
 useradd -m lx
 passwd lx
 usermod -aG sudo lx
+
+cd $HOME
+cd Wordlist
+/bin/bash Wordlists.sh
+
+cd $HOME
+rm bashrc/ && Wordlist/ && Init/ && Tools/
