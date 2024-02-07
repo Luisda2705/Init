a50918eecb098b5a6e82022e62cf5d34da66cb8a
Luisda2705 <lusartc27@gmail.com>
2024-02-07 21:38:31 +0000

    15:38:31 - 07/02/24

--- Init.sh
+++ Init.sh
@@ -2,14 +2,14 @@ cd $HOME
 git clone https://github.com/Luisda2705/bashrc.git
 git clone https://github.com/Luisda2705/Wordlist.git
 git clone https://github.com/Luisda2705/Tools.git
-git clone https://github.com/Luisda2705/Configs.git
+git clone https://github.com/Luisda2705/Setup.git
 
 useradd -m lx
 passwd lx
 usermod -aG sudo lx
 
 cp $HOME/bashrc/bashrc $HOME/.bashrc
-cp $HOME/.bashrc  /home/lx/.bashrc
+cp $HOME/bashrc/bashrc /home/lx/.bashrc
 
 /bin/bash $HOME/Tools/Tools.sh
 /bin/bash $HOME/Setup/Setup.sh
