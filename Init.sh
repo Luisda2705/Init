6354a7af63e0cca6f59d6967872d7156573924fa
Luisda2705 <lusartc27@gmail.com>
2024-10-23 14:26:01 -0600

    08:26:01 - 23/10/24

--- Init.sh
+++ Init.sh
@@ -13,6 +13,7 @@ cp $HOME/bashrc/bashrc $HOME/.bashrc
 cp $HOME/bashrc/bashrc /home/lx/.bashrc
 
 sh $HOME/Tools/Tools.sh
+sh $HOME/Setup/Setup.sh
 
 cd $HOME && rm bashrc/ && Wordlist/ && Init/ && Tools/ 
 
